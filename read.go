1ffe444a5f1f8c0316470ce33122a816e7ea8a43
drive-deep <32527844+drive-deep@users.noreply.github.com>
2020-09-09 08:14:31 +0530

    Create read.go

---
+++ read.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
+
+type name struct {
+	firstname string
+	lastname  string
+}
+
+func main() {
+
+	mySlice := make([]name, 0)
+
+	var file string
+
+	fmt.Println("enter the filename")
+	fmt.Scan(&file)
+	file, err := os.Open(file)
+
+	if err != nil {
+		log.Fatalf("failed opening file: %s", err)
+	}
+
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanLines)
+	var txtlines []string
+
+	for scanner.Scan() {
+		txtlines = append(txtlines, scanner.Text())
+	}
+
+	file.Close()
+
+	for _, eachline := range txtlines {
+		s := eachline.splt(" ")
+		var tmp name
+		tmp.firstname = s[0]
+		tmp.lastname = s[1]
+		mySlice = append(mySlice, tmp)
+	}
+	fmt.Println(mySlice)
+
+}
