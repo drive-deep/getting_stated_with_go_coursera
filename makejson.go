6c6933949dbd3ead6a3243e9223a4711458fd58d
drive-deep <32527844+drive-deep@users.noreply.github.com>
2020-09-09 08:14:04 +0530

    Create makejson.go

---
+++ makejson.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+func main() {
+
+	x := make(map[string]string)
+	var name, add string
+	fmt.Println("enter the name")
+	fmt.Scan(&name)
+	x["name"] = name
+
+	fmt.Println("enter the address")
+	fmt.Scan(&add)
+	x["address"] = add
+
+	js, _ := json.MarshalIndent(x, " ", "  ")
+	fmt.Println("-----printing the json object----")
+	print(string(js))
+}
